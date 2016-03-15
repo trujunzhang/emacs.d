9580a20e136e29daa4d8e70a8f4db87d3fbf258f
Trujun Zhang <trujunzhang@gmail.com>
2016-03-15 22:03:29 +0800

    customize mediawiki.

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mediawiki-debug t)
+ '(mediawiki-site-alist
+   (quote
+    (("djzhangwiki" "http://localhost:8888/mediawiki/" "trujunzhang" "" "" "Main Page"))))
+ '(mediawiki-site-default "djzhangwiki")
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
