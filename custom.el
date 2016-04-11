eaa9ac7c7884330ead7bc3b80df48c49547c27ac
Trujun Zhang <trujunzhang@gmail.com>
2016-04-11 16:25:16 +0800

    add ecb and projectile.

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ecb-layout-name "left9")
  '(ecb-options-version "2.40")
+ '(ecb-tip-of-the-day nil)
  '(mediawiki-debug t)
  '(mediawiki-site-alist
    (quote
